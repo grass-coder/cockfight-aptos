5dca5c274ed464bea90b74633031393e778ebe75
JSHan94 <saw151515@gmail.com>
2024-09-25 16:07:44 +0900

    chores

--- frontend/src/lib/aptosClient.ts
+++ frontend/src/lib/aptosClient.ts
@@ -5,7 +5,3 @@ const NODIT_INDEXER = NODIT_FULLNODE + "/graphql"
 
 export const aptosConfig = new AptosConfig({ network: Network.TESTNET, fullnode: NODIT_FULLNODE, indexer: NODIT_INDEXER });
 export const aptos = new Aptos(aptosConfig);
-export const TEST_APT_ADDRESS = "0xd23929f631c0ff1f0f3a152502a897c673e403c96875678f44f58f4755b9ab59"
-// const fund = await aptos.getAccountInfo({ accountAddress: "0x123" });
-// const modules = await aptos.getAccountModules({ accountAddress: "0x123" });
-// const tokens = await aptos.getAccountOwnedTokens({ accountAddress: "0x123" });
