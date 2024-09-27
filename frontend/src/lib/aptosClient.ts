7f6d5ea9734b289123b1896adfb46b72977e6d20
JSHan94 <saw151515@gmail.com>
2024-09-27 17:16:55 +0900

    test

--- frontend/src/lib/aptosClient.ts
+++ frontend/src/lib/aptosClient.ts
@@ -3,5 +3,5 @@ import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
 const NODIT_FULLNODE = "https://aptos-testnet.nodit.io/mOKySohzEyYYYL-hX4nB2P03yYqSfTHh/v1";
 const NODIT_INDEXER = NODIT_FULLNODE + "/graphql"
 
-export const aptosConfig = new AptosConfig({ network: Network.TESTNET, fullnode: NODIT_FULLNODE, indexer: NODIT_INDEXER });
+export const aptosConfig = new AptosConfig({ network: Network.TESTNET }) // , fullnode: NODIT_FULLNODE, indexer: NODIT_INDEXER });
 export const aptos = new Aptos(aptosConfig);
